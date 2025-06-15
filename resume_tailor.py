3644bca10d2a6452e158ca87f29f8b7728f1647a
ChrisPerezLebron <98716656+ChrisPerezLebron@users.noreply.github.com>
2025-06-15 12:40:40 -0400

    Added goals for this file

---
+++ resume_tailor.py
@@ -0,0 +1,5 @@
+#Goal:
+    #based on the sentence embedding of a list of skills, argsort the following resume items 
+        #related course experience
+        #work experience bullet points 
+        #projects & corresponding bullet points
